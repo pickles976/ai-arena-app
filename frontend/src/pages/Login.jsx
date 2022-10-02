1dee07ca267bbbd08fc8d74a33ae08a0f36a7463
unknown <sebaslogo@gmail.com>
2022-10-01 19:23:15 -0500

    Connnected User login stuff to frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import {useState, useEffect} from 'react'
-import {FaSignInAlt} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { useSelector, userDispatch, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaSignInAlt} from 'react-icons/fa'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -8,6 +13,28 @@ function Login() {
     })
 
     const {email, password} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    // import from global state
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    )
+
+    useEffect(() => {
+        
+        if(isError){
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -18,6 +45,17 @@ function Login() {
 
     const onSubmit = (e) => { 
         e.preventDefault()
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData))
+    }
+
+    if (isLoading){
+        return <Spinner />
     }
 
     return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
-import {useState, useEffect} from 'react'
-import {FaUser} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { useSelector, userDispatch, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser} from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +16,28 @@ function Register() {
 
     const {name, email, password, password2} = formData
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    // import from global state
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    )
+
+    useEffect(() => {
+        
+        if(isError){
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -20,6 +47,21 @@ function Register() {
 
     const onSubmit = (e) => { 
         e.preventDefault()
+
+        if(password != password2){
+            toast.error('Passwords do not match')
+        }else{
+            const userData = {
+                name,
+                email,
+                password,
+            }
+            dispatch(register(userData))
+        }
+    }
+
+    if (isLoading){
+        return <Spinner />
     }
 
     return (
@@ -58,7 +100,7 @@ function Register() {
                 </div>
                 <div className="form-group">
                     <input 
-                        type="text" 
+                        type="password" 
                         className="form-control" 
                         id='password' 
                         name='password' 
@@ -69,7 +111,7 @@ function Register() {
                 </div>
                 <div className="form-group">
                     <input 
-                        type="text" 
+                        type="password" 
                         className="form-control" 
                         id='password2' 
                         name='password2' 
