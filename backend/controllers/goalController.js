82424fe24eac474c15b564bb760b6b3f5d234138
unknown <sebaslogo@gmail.com>
2022-10-02 12:22:46 -0500

    Fully working full-stack app with auth

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,14 +40,12 @@ const updateGoals = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if (!user){
+    if (!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
-    if (goal.user.toString() !== user.id){
+    if (goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -68,14 +66,12 @@ const deleteGoals = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if (!user){
+    if (!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
-    if (goal.user.toString() !== user.id){
+    if (goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalsSlice'
+import GoalItem from '../components/GoalItem'
 
 function Dashboard() {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
+
+  useEffect(() => {
+
+    if(isError){
+      console.log(message)
+    }
+
+    // redirect to login page if not logged in
+    if(!user){
+      navigate('/login')
+    }
+
+    dispatch(getGoals())
+
+    // clear state on unmount
+    return () => {
+      dispatch(reset())
+    }
+  }
+  , [user, navigate, isError, message, dispatch])
+
+  if(isLoading){
+    return <Spinner />
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? 
+        (
+        <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+        </div>
+        ) : (
+        <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </>
   )
 }
 
