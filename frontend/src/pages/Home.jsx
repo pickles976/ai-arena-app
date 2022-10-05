7f52d0d4b028667d26a9face81171e76a16dee0e
unknown <sebaslogo@gmail.com>
2022-10-05 16:58:54 -0500

    Added homepage. Mocked layout. Updated css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,10 +8,25 @@ function Home() {
             Code Panel
         </div>
         <div className='middle-panel'>
-            Game Panel
+            <div className='game-panel'>
+                Game Panel
+            </div>
+            <div className='data-panel'>
+                <div className='gameObject-panel'>
+                    Game Object Panel
+                </div>
+                <div className='inspector-panel'>
+                    Inspector Panel
+                </div>
+            </div>
         </div>
         <div className='right-panel'>
-            Inspector Panel
+            <div className='score-panel'>
+                Score Panel
+            </div>
+            <div className='ship-panel'>
+                Ship Panel
+            </div>
         </div>
     </div>
     </>
