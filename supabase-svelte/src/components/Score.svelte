e8ef3538cfb2d1d0afd1880ee33f0cb823f84a99
unknown <sebaslogo@gmail.com>
2023-02-20 09:08:11 -0600

    Cleaned up the score component

--- supabase-svelte/src/components/Score.svelte
+++ supabase-svelte/src/components/Score.svelte
@@ -1,22 +1,101 @@
 <script>
   import { gameData } from "../stores";
-  $: team0 = $gameData.score["team 0"]
-  $: team1 = $gameData.score["team 1"]
+  $: team0 = $gameData.score["team 0"];
+  $: team1 = $gameData.score["team 1"];
 </script>
-  
-<div class="hor-panel">
-    {#if team0}
-      Team 0 <br>
-      kills: {team0.kills} <br>
-      deaths: {team0.deaths} <br>
-      metal: {team0.metal} <br>
-      energy: {team0.energy} <br>
-    {/if}
-    {#if team1}
-      Team 1 <br>
-      kills: {team1.kills} <br>
-      deaths: {team1.deaths} <br>
-      metal: {team1.metal} <br>
-      energy: {team1.energy} <br>
-    {/if}
+
+<div class="score-panel">
+  <div class="teams-info" id="teams-info">
+    <div class="container row">
+      <div class="col-left">
+        <div class="cell">Values</div>
+        <div class="cell">Kills</div>
+        <div class="cell">Deaths</div>
+        <div class="cell">Metal</div>
+        <div class="cell">Energy</div>
+      </div>
+      <div class="col-middle">
+        {#if team0}
+          <div class="cell">Team 0</div>
+          <div class="cell">{team0.kills}</div>
+          <div class="cell">{team0.deaths}</div>
+          <div class="cell">{team0.metal}</div>
+          <div class="cell">{team0.energy}</div>
+        {/if}
+      </div>
+      <div class="col-middle">
+        {#if team0}
+          <div class="cell">Team 1</div>
+          <div class="cell">{team1.kills}</div>
+          <div class="cell">{team1.deaths}</div>
+          <div class="cell">{team1.metal}</div>
+          <div class="cell">{team1.energy}</div>
+        {/if}
+      </div>
+    </div>
+  </div>
 </div>
+
+<style>
+  .score-panel {
+    height: 12.5vh;
+    font-size: 16px;
+    font-size: 1.7vmin;
+    background-color: var(--secondary);
+  }
+
+  .teams-info {
+    padding: 8px;
+  }
+
+  .container {
+    width: 100%;
+    height: 100%;
+    border: 0;
+    margin: 0;
+    display: flex;
+  }
+
+  .row {
+    flex-direction: row;
+  }
+
+  .cell {
+    /* border:1px solid white; */
+    background-color: var(--primary);
+    margin: 5%;
+  }
+
+  .col-left {
+    width: 33%;
+    height: 100%;
+    border-collapse: collapse;
+    border: 0;
+    margin: 0;
+    float: left;
+    padding: 2px;
+    white-space: nowrap;
+  }
+
+  .col-middle {
+    width: 33%;
+    height: 100%;
+    border-collapse: collapse;
+    border: 0;
+    margin: 0;
+    float: left;
+    padding: 2px;
+    white-space: nowrap;
+  }
+
+  .col-right {
+    width: 33%;
+    height: 100%;
+    border-collapse: collapse;
+    border: 0;
+    margin: 0;
+    float: left;
+    padding: 2px;
+    white-space: nowrap;
+  }
+</style>
